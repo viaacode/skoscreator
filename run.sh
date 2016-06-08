e3e03689ec4c4f22d79231d0d22b9f95bbaf8fa5
Ruben Taelman <ruben.taelman@ugent.be>
2016-06-08 14:21:24 +0200

    Cleanup code and add inline documentation

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-bin/mapper > thesaurus.ttl
+# Converts the input thesaurus xml file to skos in turtle serialization and cleans it up using skosify
+
+if [ $# -eq 0 ]; then
+    echo "Please provide the path to a thesaurus xml file"
+    exit 1
+fi
+
+file=$1
+bin/mapper "$file" > thesaurus.ttl
 >&2 echo "> Cleaning up..."
 skosify thesaurus.ttl -o thesaurus-clean.ttl --label "VRT Thesaurus"
 >&2 echo "> Done!"
+
